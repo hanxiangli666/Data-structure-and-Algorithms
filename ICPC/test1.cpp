fb17977e7bbd3d9148a45e1a51ee8bd35e35d83d
James <hanxiangli666@gmail.com>
2026-03-16 18:49:18 -0400

    clean up my house

--- ICPC/test1.cpp
+++ ICPC/test1.cpp
@@ -2,39 +2,31 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
 #include <numeric>
 #include <cmath>
 using namespace std;
 
 typedef long long ll;
-
 const int MOD = 1e9 + 7;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-
-
-    return 0;
+ll power(ll a, ll b, ll mod) {
+    ll result = 1;
+    a %= mod;
+    while (b > 0) {
+        if (b & 1) result = result * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return result;
 }
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <numeric>
-#include <cmath>
-using namespace std;
-
-typedef long long ll;
-
-const int MOD = 1e9 + 7;
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
 
 
     return 0;
